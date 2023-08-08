83538fec4a7442d01bf4f186fcf4a07c3b4e6628
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-08-08 01:23:23 -0300

    ultimo commit

---
+++ texto.js
@@ -0,0 +1,10 @@
+
+// para hacer animacion ,preguntar a matias lorenzo
+var typed2 = new Typed('#typed2', {
+    strings: ['<i>"Soy de Maria Juana ,una pequeña localidad de la provincia de Santa Fe,empece en el 2022 con codo a codo inical en python este año segui con el full stack en dicho lenguaje tambien y ahora busco especializarme en el fron end con React "</i>'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    showCursor: false
+  });
